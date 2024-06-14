bbf2ddcbe0cbae4ddbdb98abae879a209cba171c
garywei944 <33930674+garywei944@users.noreply.github.com>
2024-06-14 00:05:04 -0400

    Format everything

--- assets/js/contact-form.js
+++ assets/js/contact-form.js
@@ -1,5 +1,5 @@
 $(function () {
-  let form = document.getElementById("contactForm");
+  let form = document.getElementById('contactForm');
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -8,24 +8,24 @@ $(function () {
       method: form.method,
       body: data,
       headers: {
-        Accept: "application/json",
+        Accept: 'application/json',
       },
     })
       .then((response) => {
-        $("#form-message-warning").hide();
+        $('#form-message-warning').hide();
         setTimeout(function () {
-          $("#contactForm").fadeOut();
+          $('#contactForm').fadeOut();
         }, 1000);
         setTimeout(function () {
-          $("#form-message-success").fadeIn();
+          $('#form-message-success').fadeIn();
         }, 1400);
       })
       .catch((error) => {
-        $("#form-message-warning").html("Something went wrong. Please try again.");
-        $("#form-message-warning").fadeIn();
-        $submit.css("display", "none");
+        $('#form-message-warning').html('Something went wrong. Please try again.');
+        $('#form-message-warning').fadeIn();
+        $submit.css('display', 'none');
       });
   }
 
-  form.addEventListener("submit", handleSubmit);
+  form.addEventListener('submit', handleSubmit);
 });
